068f7ff39fa7cf066bc42cc4ea5cd6327fa8b859
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-01 21:25:42 +0300

    виконала 9 таск

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+// функція рандомних кольорів
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+// змінні для тегів
+const bodyEl = document.querySelector('body')
+
+const btnChangeColor = document.querySelector('.change-color');
+
+const spanEl = document.querySelector('.color');
+
+// вішаємо слухача і через стрілочну функцію присвоюємо бекграунду значення функції рандомних кольорів, і це ж значення присвоюємо текстовому контенту спана
+btnChangeColor.addEventListener('click', event => {
+  bodyEl.style.background = getRandomHexColor();
+
+  spanEl.textContent = getRandomHexColor();
+});
+
+
