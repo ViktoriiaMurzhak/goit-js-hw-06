5e35236c9b20bbb4f2d90f50b2506ca3153a5943
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-06 16:35:08 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,10 @@
 // змінні для тегів
 const formEl = document.querySelector('.login-form');
-
 const inputEmailEl = document.querySelector('input[type=email]');
-
 const inputPasswordEl = document.querySelector('input[type=password]');
 
-// створюємо формулу для нового масиву, який приймає значення полів форм, та не перезавантажує сторінку після submit
+// створюємо формулу для нового масиву, який приймає значення полів форм, та не перезавантажує сторінку після submit, та виводить попередження про заповнення усіх полів, якщо вони пусті
+
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
@@ -14,15 +13,14 @@ function onFormSubmit(event) {
   const formData = new FormData(event.currentTarget);
   formData.email = inputEmailEl.value;
   formData.password = inputPasswordEl.value;
-console.log(formData);
- 
-};
+  console.log(formData);
 
-// створюємо форму для виведення алертів, якщо якесь поле пусте
-formEl.addEventListener('submit', alertCall);
-
-function alertCall(event) {
+  // для виведення алертів, якщо якесь поле пусте
   if (inputEmailEl.value === '' || inputPasswordEl.value === '') {
     alert('Заповніть усі поля!');
-  } 
-};
+  }
+
+   event.currentTarget.reset();
+
+}
+
