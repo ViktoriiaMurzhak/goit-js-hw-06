ae584bbb6cd2c4af079e4b11a09559736730813a
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-06 18:05:36 +0300

    Переробила 6 таск після перевірки

--- js/task-06.js
+++ js/task-06.js
@@ -1,25 +1,19 @@
-// змінна для тега input
+// змінна для тега input;
 const inputEl = document.querySelector('#validation-input');
 
-console.log(inputEl);
-
-// вішаємо слухача на блюр (прибирання фокусу)
-
+// вішаємо слухача на блюр (прибирання фокусу);
 inputEl.addEventListener('blur', onInputBlur);
 
-// пишемо функцію: якщо довжина значення інпуту, яка приведена до рядко строго = значенню інпут ключа data-length - додай клас valid i видали invalid, якщо він там був. В іншому випадку зроби навпаки
-
+// пишемо функцію: якщо довжина значення інпуту, яка приведена до рядко строго = значенню інпут ключа data-length - додай клас valid i видали invalid, якщо він там був. В іншому випадку зроби навпаки;
 function onInputBlur(event) {
   if (event.currentTarget.value.length.toString() === inputEl.dataset.length) {
-     inputEl.classList.add('valid');
-     inputEl.classList.remove('invalid');
+   updateClas('valid', 'invalid');
   } else {
-     inputEl.classList.add('invalid');
-     inputEl.classList.remove('valid');
-}
+   updateClas('invalid', 'valid');
+  }
 };
 
-
-
-
-
+function updateClas(clas1, clas2) {
+  inputEl.classList.add(clas1);
+  inputEl.classList.remove(clas2);
+};
